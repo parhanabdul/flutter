874430420aa3078b7268ceb1e6c4674f6983afd9
paarhan <anparhan33@gmail.com>
2021-02-08 16:58:14 -1200

    belajar

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+ return MaterialApp(
+   debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tracking COVID-19'),
+        ),
+        body: Center(
+          child: Text('Parhan yoyo'),
+      ),
+      ),
+    );
+  }
+}
+
